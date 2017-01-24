0fa4d6115c1553e93edbeb64e68750ef35cb0a37
LORD-KAY <offeilord@gmail.com>
2017-01-24 20:39:55 +0000

    Edit Css,Images and Add new content for the index/home page

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.slider').slider({
         full_width:true,
-        height:500,
-        transition:200,
-        interval:4000
+        height:570
     });
 });
